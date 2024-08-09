9c75b756e4e0ce727ecd258ac687ad5be030ca6b
Stanislav Lyakhov <stanislav@galois.com>
2024-08-09 13:36:00 -0700

    Adjust install_driver for submodule

--- tools/install_driver.sh
+++ tools/install_driver.sh
@@ -3,12 +3,27 @@
 set -euo pipefail
 
 thisdir=$(dirname -- "$0")
-prefix=$(mktemp -d)
 
 build="${1}"
 driver_guid="${2}"
 base="${3}"
 output_rom="${4}"
 
-${thisdir}/UEFIToolNew/extract_file.sh "${build}" "${driver_guid}" "${prefix}"
-${thisdir}/UEFITool28/insert_dxe.sh "${base}" "${prefix}/${driver_guid}.bin/file.ffs" "${output_rom}"
+extracted=$(mktemp -d)
+extract_file="${extracted}/${driver_guid}.ffs"
+
+uefiextract=${thisdir}/UEFITool/uefiextract
+uefiinsert=${thisdir}/UEFITool28/insert_dxe.sh
+
+if ! test -f ${uefiextract}; then
+    echo "UEFIExtract not found: make sure to build it first"
+    exit 1
+fi
+
+if ! test -f ${uefiinsert}; then
+    echo "UEFIInsert not found: make sure to build it first"
+    exit 1
+fi
+
+${uefiextract} "${build}" "${driver_guid}" -o ${extract_file} -m file
+${uefiinsert} "${base}" "${extract_file}/file.ffs" "${output_rom}"
